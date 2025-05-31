8ddd92c45c1af949f18e26f7f44e490ac3014365
dominic <git@domi.ninja>
2025-05-31 20:13:41 +0000

    cleanup

--- src/components/FileManageTable.tsx
+++ src/components/FileManageTable.tsx
@@ -647,17 +647,8 @@ export function FileManageTable() {
                 <div className="grid grid-cols-2 md:grid-cols-3 items-center mb-4 gap-4">
                     <div className="flex items-center gap-2">
                         <h2 className={`text-2xl font-semibold cursor-pointer p-2`} onClick={() => setSelectedFiles(new Set())}>
-                            {(currentFolderId === rootFolderId ?
-                                (<span>
-                                    <span className="pr-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 inline-block">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
-                                        </svg>
-                                    </span>
-                                    Files
-                                </span>)
-                                :
-                                (<span>
+                            {(
+                                <span>
                                     {currentFolderPath?.map((folder, fidx) => (
                                         <span key={folder._id}>
                                             <span className="hover:text-blue-600 cursor-pointer" onClick={() => setCurrentFolderId(folder._id)}>
@@ -688,7 +679,7 @@ export function FileManageTable() {
                                             )}
                                         </span>
                                     ))}
-                                </span>)
+                                </span>
                             )}
                         </h2>
 
