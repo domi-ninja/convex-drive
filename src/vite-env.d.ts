cc0a85005bf3774002b38e7f18057275b230cb5d
dominic <git@domi.ninja>
2025-06-05 09:52:52 +0000

    fix prod env var 4

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="vite/types/importMeta.d.ts" />
+
+interface ImportMetaEnv {
+    readonly VITE_CONVEX_URL: string;
+    // Add other environment variables you expect here
+    // readonly VITE_SOME_OTHER_VAR: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
